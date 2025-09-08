85a2d68b2824557c9d16f2fc18ce15a0de8d3c11
Srini Arul David <srinivasan.arulsivam@macys.com>
2025-09-08 14:59:50 -0700

    Initial Commit

---
+++ Lab1/Lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1App.swift
+//  Lab1
+//
+//  Created by Srini Arul David on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab1Tests/Lab1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1Tests.swift
+//  Lab1Tests
+//
+//  Created by Srini Arul David on 9/8/25.
+//
+
+import Testing
+@testable import Lab1
+
+struct Lab1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
